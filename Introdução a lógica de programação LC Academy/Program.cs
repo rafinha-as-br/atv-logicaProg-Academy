b526840bde9056b71ab557599166d1cbae8184df
Rafael Antunes <rafinha72465@gmail.com>
2025-03-19 20:24:11 -0300

    Questão 1 concluída!

--- Introdução a lógica de programação LC Academy/Program.cs
+++ Introdução a lógica de programação LC Academy/Program.cs
@@ -1,1 +1,17 @@
-﻿int 
+﻿float numero1, numero2, resultado;
+
+Console.WriteLine("Informe o primeiro número decimal:");
+numero1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo número decimal:");
+numero2 = float.Parse(Console.ReadLine());
+
+if (numero1 > numero2)
+{
+    resultado = numero1 / numero2;
+    Console.WriteLine("O resultado da divisão do maior número pelo menor é: " + resultado);
+}
+else
+{
+    resultado = numero2 / numero1;
+    Console.WriteLine("O resultado da divisão do maior número pelo menor é: " + resultado);
+}
