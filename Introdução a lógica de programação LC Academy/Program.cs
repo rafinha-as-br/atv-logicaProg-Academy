a379a9dd6954b974eacafc212e9fa1a5824d8fb5
Rafael Antunes <rafinha72465@gmail.com>
2025-03-19 20:33:50 -0300

    Questão 2 concluída!

--- Introdução a lógica de programação LC Academy/Program.cs
+++ Introdução a lógica de programação LC Academy/Program.cs
@@ -1,1 +1,16 @@
-﻿int 
+﻿int[] numeros = new int[4];
+
+for (int i = 0; i < 4; i++)
+{
+    Console.WriteLine("Digite um número:");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("\nNúmeros ímpares digitados:");
+for (int i = 0; i < 4; i++)
+{
+    if (numeros[i] % 2 != 0)
+    {
+        Console.WriteLine(numeros[i]);
+    }
+}
