12e3275185a73d698f6335f0b5eeb17164b27c06
Rafael Antunes <rafinha72465@gmail.com>
2025-03-20 13:45:21 -0300

    Questão 6 concluída!

--- Introdução a lógica de programação LC Academy/Program.cs
+++ Introdução a lógica de programação LC Academy/Program.cs
@@ -1,29 +1,11 @@
-﻿int a, b, c, x, x1,x2;
-float resultado;
-int delta;
+﻿double cat1, cat2;
+double hipotenusa;
 
-Console.WriteLine("Digite o valor de A: ");
-a = int.Parse(Console.ReadLine());
-Console.WriteLine("Digite o valor de B: ");
-b = int.Parse(Console.ReadLine());
-Console.WriteLine("Digite o valor de C: ");
-c = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor do cateto 1: ");
+cat1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor do cateto 2: ");
+cat2 = int.Parse(Console.ReadLine());
 
-delta = b* b - 4 * a * c;
+hipotenusa = Math.Sqrt((cat1 * cat1) + (cat2 * cat2));
 
-if(delta > 0)
-{
-    x1 = (-b + (int)Math.Sqrt(delta)) / (2 * a);
-    x2 = (-b - (int)Math.Sqrt(delta)) / (2 * a);
-    Console.WriteLine("Trata-se de duas raízes, portanto: ");
-    Console.WriteLine("O X1 é: " + x1);
-    Console.WriteLine("O X2 é: " + x2);
-} else if (delta == 0)
-{
-    x = -b / (2 * a);
-    Console.WriteLine("Trata-se de uma raiz única, portanto o X é: " + x);
-
-} else
-{
-    Console.WriteLine("Não existe raiz real");
-}
+Console.WriteLine("O valor da hipotenusa é: " + hipotenusa);
