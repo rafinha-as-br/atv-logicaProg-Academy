2819a7d34fa1560e375e9ef710871cc4407c5509
Rafael Antunes <rafinha72465@gmail.com>
2025-03-20 19:24:45 -0300

    commit2

--- Introdução a lógica de programação LC Academy/Program.cs
+++ Introdução a lógica de programação LC Academy/Program.cs
@@ -1,11 +1,17 @@
-﻿double cat1, cat2;
+﻿//Questão 6
+
+//cria as variáveis necessárias
+double cat1, cat2;
 double hipotenusa;
 
+//entrada de dados
 Console.WriteLine("Digite o valor do cateto 1: ");
 cat1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Digite o valor do cateto 2: ");
 cat2 = int.Parse(Console.ReadLine());
 
+//calculo da hipotenusa (processamento)
 hipotenusa = Math.Sqrt((cat1 * cat1) + (cat2 * cat2));
 
+//impressão do resultado
 Console.WriteLine("O valor da hipotenusa é: " + hipotenusa);
