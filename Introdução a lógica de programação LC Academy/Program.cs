3e695e3a9a31dff0cb0a91561f41d7918b1c254a
Rafael Antunes <rafinha72465@gmail.com>
2025-03-20 19:40:38 -0300

    Exercício 8 concluído!

--- Introdução a lógica de programação LC Academy/Program.cs
+++ Introdução a lógica de programação LC Academy/Program.cs
@@ -1,8 +1,13 @@
-﻿int numero, resultado=0;
+﻿//Questão 8
 
+//cria as variavéis necessárias
+int numero, resultado=0;
+
+//entrada de dados 
 Console.WriteLine("Digite um número: ");
 numero = int.Parse(Console.ReadLine());
 
+//realização e impressão do resultado (tabuada até o 9)
 for(int i=1; i <=9; i++)
 {
     resultado = numero * i;
