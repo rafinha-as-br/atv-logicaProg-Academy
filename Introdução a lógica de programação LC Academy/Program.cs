986f8c34f1ee36aa0e4326b6205e9c8ede62d89b
Rafael Antunes <rafinha72465@gmail.com>
2025-03-20 14:58:19 -0300

    Questão 3 concluída!

--- Introdução a lógica de programação LC Academy/Program.cs
+++ Introdução a lógica de programação LC Academy/Program.cs
@@ -1,8 +1,21 @@
-﻿int numero1, numero2, resultado;
+﻿//Questão 3
+
+//cria as variáveis necessárias
+int numero1, numero2, resultado;
+
+//solicita a entrada dos números
 Console.WriteLine("Digite o primeiro número: ");
 numero1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Digite o segundo número: ");
 numero2 = int.Parse(Console.ReadLine());
 
-resultado = Math.Abs(numero2 - numero1);
+//realiza a subtração
+resultado = numero1 - numero2;
+
+//verifica se a subtração é negativa e transforma em positiva
+if(resultado > 0)
+{
+    resultado = resultado * -1;
+}
+
 Console.WriteLine("O valor absoluto desta subtração é: " + resultado);
