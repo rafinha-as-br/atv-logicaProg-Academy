da3b5a20eeefdc22bf4d7e69f33ea22f388748fd
Rafael Antunes <rafinha72465@gmail.com>
2025-03-20 14:34:54 -0300

    Questão 9 concluída!

--- Introdução a lógica de programação LC Academy/Program.cs
+++ Introdução a lógica de programação LC Academy/Program.cs
@@ -1,10 +1,39 @@
-﻿int numero, resultado=0;
+﻿//cria as variáveis
+string texto;
+int vogais=0, consoantes=0, palavras = 0;
 
-Console.WriteLine("Digite um número: ");
-numero = int.Parse(Console.ReadLine());
+//pede entrada de dados do usuário e armazena na variável texto
+Console.WriteLine("Digite um texto: ");
+texto = Console.ReadLine();
 
-for(int i=1; i <=9; i++)
+//loop que percorre a string texto e vai obtendo todas as informações
+for (int i = 0; i < texto.Length; i++)
 {
-    resultado = numero * i;
-    Console.WriteLine($"{numero} x {i} = {resultado}");
+    //verifica os espaços
+    if (texto[i] == ' ')
+    {
+        palavras++;
+    }
+
+    //verifica se é vogal ou consoante
+    else if (texto[i] == 'a' || texto[i] == 'e' || texto[i] == 'i' || texto[i] == 'o' || texto[i] == 'u')
+    {
+        vogais++;
+    }
+    else
+    {
+        consoantes++;
+    }
 }
+
+//imprime as informações
+Console.WriteLine("Quantidade de vogais: " + vogais);
+Console.WriteLine("Quantidade de consoantes: " + consoantes);
+Console.WriteLine("Quantidade de letras: " + (vogais+consoantes));
+Console.WriteLine("Quantidade de palavras: " + (palavras + 1));
+
+
+
+
+
+
