6f8b36fcd344e76c2b7d53fb5780517d740ac78a
Rafael Antunes <rafinha72465@gmail.com>
2025-03-20 14:44:05 -0300

    Questão 2 concluída!

--- Introdução a lógica de programação LC Academy/Program.cs
+++ Introdução a lógica de programação LC Academy/Program.cs
@@ -1,12 +1,20 @@
-﻿int[] numeros = new int[5];
+﻿//Questão 2
 
+//Cria a variável vetor de 5 posições
+int[] numeros = new int[5];
+
+
+//preenche o vetor com os números do usuário
 for (int i = 0; i < 4; i++)
 {
     Console.WriteLine("Digite um número:");
     numeros[i] = int.Parse(Console.ReadLine());
 }
 
+
 Console.WriteLine("\nNúmeros ímpares digitados:");
+
+//verifica quais números são ímpares e imprime
 for (int i = 0; i < 4; i++)
 {
     if (numeros[i] % 2 != 0)
