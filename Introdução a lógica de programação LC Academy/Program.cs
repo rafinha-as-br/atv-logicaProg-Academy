577a11672e40e97ed3801c9151ca53ae9542d132
Rafael Antunes <rafinha72465@gmail.com>
2025-03-19 21:01:46 -0300

    Exercício 4 concluído!

--- Introdução a lógica de programação LC Academy/Program.cs
+++ Introdução a lógica de programação LC Academy/Program.cs
@@ -1,8 +1,28 @@
-﻿int numero1, numero2, resultado;
+﻿float numero1, numero2;
+int num1,num2, resultado;
 Console.WriteLine("Digite o primeiro número: ");
-numero1 = int.Parse(Console.ReadLine());
+numero1 = float.Parse(Console.ReadLine());
 Console.WriteLine("Digite o segundo número: ");
-numero2 = int.Parse(Console.ReadLine());
+numero2 = float.Parse(Console.ReadLine());
 
-resultado = Math.Abs(numero2 - numero1);
-Console.WriteLine("O valor absoluto desta subtração é: " + resultado);
+num1 = (int)Math.Round(numero1);
+num2 = (int)Math.Round(numero2);
+
+resultado = CalcularMMC(num1, num2); 
+
+Console.WriteLine($"O MMC de {num1} e {num2} é {resultado}");
+
+static int CalcularMMC(int a, int b)
+{
+    return (a * b) / CalcularMDC(a, b);
+}
+static int CalcularMDC(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
